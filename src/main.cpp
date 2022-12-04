44b01285b75cc5ec91799e3b31f4058a51f1ee6a
honey the codewitch <gaztoast@gmail.com>
2022-12-04 04:22:52 -0800

    progress!

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <htcw_button.hpp>
 #include <st7789.hpp>
 #include <tft_io.hpp>
+#include <lcd_miser.hpp>
 #include <fonts/Ubuntu.hpp>
 #include <SPIFFS.h>
 #define LCD_WIDTH 135
@@ -20,24 +21,27 @@ using namespace arduino;
 using namespace gfx;
 
 using bus_t = tft_spi_ex<LCD_HOST, PIN_NUM_CS, PIN_NUM_MOSI, PIN_NUM_MISO, PIN_NUM_CLK, SPI_MODE0,true,LCD_WIDTH*LCD_HEIGHT*2+8,2>;
-using display_t = st7789<LCD_WIDTH, LCD_HEIGHT, PIN_NUM_DC, PIN_NUM_RST, PIN_NUM_BCKL, bus_t, 1, true, 400, 200>;
+using display_t = st7789<LCD_WIDTH, LCD_HEIGHT, PIN_NUM_DC, PIN_NUM_RST, -1 /* PIN_NUM_BCKL */, bus_t, 1, true, 400, 200>;
 using color_t = color<typename display_t::pixel_type>;
 
 using button_1_t = button<35, 10, true>;
 using button_2_t = button<0, 10, true>;
 
 static void draw_room(int index);
-
+static void play_pause(int index);
 static display_t dsp;
-
+lcd_miser<PIN_NUM_BCKL> dsp_miser;
 static button_1_t button_1;
 static button_2_t button_2;
 
 static int speaker_index = 0;
 static int speaker_count = 0;
 static char* speaker_strings = nullptr;
+static char play_pause_url[1024];
+static char url[1024];
 static void button_1_cb(bool pressed, void* state) {
     if(pressed) {
+        dsp_miser.wake();
         ++speaker_index;
         if(speaker_index>=speaker_count) {
             speaker_index = 0;
@@ -46,7 +50,10 @@ static void button_1_cb(bool pressed, void* state) {
     }
 }
 static void button_2_cb(bool pressed, void* state) {
-    Serial.printf("Button 2 %s\n",pressed?"pressed":"released");
+    if(pressed) {
+        dsp_miser.wake();
+        play_pause(speaker_index);
+    }
 }
 static void draw_center_text(const char* text) {
     open_text_info oti;
@@ -62,17 +69,29 @@ static void draw_center_text(const char* text) {
     draw::text(dsp,text_rect,oti,color_t::white);
 
 }
-static void draw_room(int index) {
-    draw::filled_rectangle(dsp, dsp.bounds(), color_t::black);
+static const char* room_for_index(int index) {
     const char* sz = speaker_strings;
     for(int i = 0;i<index;++i) {
         sz = sz+strlen(sz)+1;
     }
+    return sz;
+}
+
+static void play_pause(int index) {
+    const char* sz = room_for_index(index);
+    snprintf(url,sizeof(url),play_pause_url,sz);
+    Serial.print("Sending ");
+    Serial.println(url);
+}
+static void draw_room(int index) {
+    draw::filled_rectangle(dsp, dsp.bounds(), color_t::black);
+    const char* sz = room_for_index(index);
     draw_center_text(sz);
 }
 void setup() {
     Serial.begin(115200);
     SPIFFS.begin();
+    dsp_miser.initialize();
     button_1.initialize();
     button_2.initialize();
     button_1.callback(button_1_cb);
@@ -100,11 +119,17 @@ void setup() {
         ++speaker_count;
     }
     file.close();
+    file = SPIFFS.open("/api.txt");
+    s = file.readStringUntil('\n');
+    Serial.println(s);
+    s.trim();
+    strcpy(play_pause_url,s.c_str());
+    file.close();
     draw_room(speaker_index);
 }
 void loop() {
     
-
+    dsp_miser.update();
     button_1.update();
     button_2.update();
 }
